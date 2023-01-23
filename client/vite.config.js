806ed37cadf129b5f1acb21091a32fe608760c49
Eldar Nir <loshinas@gmail.com>
2023-01-23 18:01:50 +0200

    viteconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-module.exports = {
   build: {
-      rollupOptions: {
-          external: ['react-router-dom']
-      }
+    rollupOptions: {
+        external: ['react-router-dom']
+    }
   }
-}
+})
