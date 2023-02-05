95a53bbe5a70c777139a886d40f3a9dbff9d21f7
Eldar Nir <loshinas@gmail.com>
2023-02-05 21:54:42 +0200

    adding audio field to server

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,29 +13,34 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 router.route('/').get((req, res) => {
-    res.send('Hello from DALL-E!')
+    res.send('Hello from DALL-E and GPT-3!')
 })
 
 router.route('/').post(async (req, res) => {
     try {
         const { prompt, size } = req.body
 
-        const aiResponse = await openai.createImage({
+        const imageResponse = await openai.createImage({
             prompt,
             n: 1,
             size,
             response_format: 'b64_json'
         })
 
-        const image = aiResponse.data.data[0].b64_json
-        
-        res.status(200).json({ photo: image})
-        console.log('!!!!!!!!!!!!!!!!!! ', req.body);
+        const image = imageResponse.data.data[0].b64_json
 
+        const audioResponse = await openai.textToSpeech({
+            text: prompt,
+            response_format: 'mp3'
+        })
+
+        const audio = audioResponse.data.data[0].audio
+
+        res.status(200).json({ photo: image, audio: audio })
     } catch (error) {
         console.log('Something went wrong ', error)
         res.status(500).send(error?.response.data.error.message)
     }
 })
 
-export default router
+export default router 
